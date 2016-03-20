ce090454fef1802b644adcaa2bb7775cb10b41a3
Ravi Teja <rachanti@factset.com>
2016-03-21 01:16:19 +0530

    files

---
+++ Routes_Config_Views- 1/Routes_Config_Views/Scripts/MainModule.js
@@ -0,0 +1,38 @@
+﻿var MainModule = angular.module('MainModule', ['ngRoute']);
+
+MainModule.controller('Controller1', function ($scope)
+{
+    var d = new Date(Date.now()-5);
+    $scope.ControllerName = "Controller Name: Controller1 - " + d;
+});
+
+MainModule.controller('Controller2', function ($scope)
+{
+    var d = new Date(Date.now());
+    $scope.ControllerName = "Controller Name: Controller2 - " + d;
+});
+
+MainModule.config(function ($routeProvider)
+{
+    $routeProvider
+        .when('/',
+            {
+                templateUrl: 'Views/View1.html',
+                controller: 'Controller1'
+            })
+        .when('/View2',
+            {
+                templateUrl: 'Views/View2.html',
+                controller: 'Controller1'
+            })
+        .when('/View3',
+            {
+                templateUrl: 'Views/View3.html',
+                controller: 'Controller2'
+            })
+        .otherwise(
+            {
+                redirectTo: '/'
+            })
+});
+
---
+++ Routes_Config_Views- 2/Routes_Config_Views/Scripts/MainModule.js
@@ -0,0 +1,39 @@
+﻿
+// Steps:
+// First Add Query String Key Name in Routes
+// Second Add Query String Value in html file 
+// Modify Controller to access query string parameter using $routeParams
+
+var MainModule = angular.module('MainModule', ['ngRoute']);
+
+MainModule.controller('Controller1', function ($scope)
+{
+    $scope.ControllerName = "Controller which doesn't handle QueryString";
+});
+
+MainModule.controller('Controller2', function ($scope, $routeParams)
+{
+    $scope.ControllerName = "Data Passed in Query String: " + $routeParams.QSParam;
+});
+
+
+MainModule.config(function ($routeProvider)
+{
+    $routeProvider
+        .when('/',
+            {
+                templateUrl: 'Views/View1.html',
+                controller: 'Controller1'
+            })
+        .when('/View2/:QSParam',
+            {
+                templateUrl: 'Views/View2.html',
+                controller: 'Controller2'
+            })
+        .otherwise(
+            {
+                redirectTo: '/'
+            })
+});
+
+///:firstName/:middleName/:lastName
